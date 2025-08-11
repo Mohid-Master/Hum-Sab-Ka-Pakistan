2f7bb8a18cdb0995b06b6bd85adb67c3d90f9864
amohid751 <amohid751@gmail.com>
2025-08-11 04:42:35 +0000

    added vecrel speed insight <SpeeedInsight tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,6 +41,7 @@ export default function RootLayout({
         <Suspense fallback={<Loading />}>
         {children}
         </Suspense>
+        <SpeedInsights />
       </body>
     </html>
   );
